6890b495bdcfeb0d3016d84af26d9906ac4639c1
mahmud <mahmudskarim@gmail.com>
2021-11-25 16:01:54 -0700

    setup the basic template for the player page

--- src/pages/Playerpage.jsx
+++ src/pages/Playerpage.jsx
@@ -1,7 +1,23 @@
+import TextField from "@mui/material/TextField";
+import React from "react";
+
 export default function Playerpage() {
-    return (
-      <div>
-        <h1>Player Page </h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Player Page </h1>
+      <label>Big Buck Bunny</label>
+      <br />
+      <video width="640" height="360" controls>
+        <source src="https://ipfs.fleek.co/ipfs/QmbGtJg23skhvFmu9mJiePVByhfzu5rwo74MEkVDYAmF5T"></source>
+        Your browser does not support the video tag.
+      </video>
+      <br />
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Enter You IPFS CID"
+        variant="outlined"
+      />
+    </div>
+  );
+}
